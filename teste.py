c438bf7f56e092c551e11485a8f582ef2d0d39d3
VicthorSousa <89492328+VicthorSousa@users.noreply.github.com>
2024-03-06 16:56:00 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pandas as pd
+
+pd.options.display.float_format = '{:.0f}'.format
+
+df = pd.read_csv('cupons_janeiro_filial.csv', on_bad_lines='skip', sep=';')
+produto_lista = df['Produto'].sort_values().unique()
+
+st.title(':bar_chart: Análise de Produtos - Coocorrência - Loja Estádio')
+option = st.selectbox(label='Selecione o produto', options=produto_lista, placeholder='Selecione o produto...')
+#nome_produto = st.markdown(sorted(option))
+
+
+
+qnt_total = df['CUPNUM'].nunique()                      
+df1 = df.loc[(df['Produto']==option), ['CUPNUM','CODPRD', 'Produto', 'Qnt Venda', 'Total']]
+cupons_lista = df1['CUPNUM'].tolist()
+new_df = df.loc[df['CUPNUM'].isin(cupons_lista)]
+quantidade = new_df['CUPNUM'].nunique()
+new_df1 = new_df.groupby('Produto').nunique().sort_values('Qnt Venda')
+new_df1.rename(columns={'CUPNUM': 'Qnt Cupons'})
+new_df2 = new_df1.sort_values(('CUPNUM'), ascending=False).head(500)
+new_df2['Total'] = new_df['CUPNUM'].nunique()
+new_df2['Percentual'] = (new_df2['CUPNUM'] / new_df2['Total'] * 100).round(2).astype(str) + '%'
+new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CUPNUM'], axis=1, inplace=True)
+
+st.text(f'Analise de {quantidade} cupons emitidos do produto selecionado.')
+st.text('Dados referentes a venda de Janeiro/2024')
+st.dataframe(new_df2, use_container_width=True)
