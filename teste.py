7de7cf852995feb31be3e6e106ec7c0deb905338
VicthorSousa <89492328+VicthorSousa@users.noreply.github.com>
2024-03-13 17:10:09 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,17 +1,14 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 pd.options.display.float_format = '{:.0f}'.format
-
-st.set_page_config(page_title='Análise de Coocorrência', page_icon=':bar_chart:')
 
 df = pd.read_csv('cupons_janeiro_filial.csv', on_bad_lines='skip', sep=';')
 produto_lista = df['Produto'].sort_values().unique()
 
 st.title(':bar_chart: Análise de Produtos - Coocorrência - Loja Estádio')
 option = st.selectbox(label='Selecione o produto', options=produto_lista, placeholder='Selecione o produto...')
-#nome_produto = st.markdown(sorted(option))
-
 
 
 qnt_total = df['CUPNUM'].nunique()                      
@@ -26,6 +23,8 @@ new_df2['Total'] = new_df['CUPNUM'].nunique()
 new_df2['Percentual'] = (new_df2['CUPNUM'] / new_df2['Total'] * 100).round(2).astype(str) + '%'
 new_df2.drop(['Qnt Venda', 'Total', 'CODPRD', 'CUPNUM'], axis=1, inplace=True)
 
-st.text(f'Analise de {quantidade} cupons emitidos do produto selecionado.')
-st.text('Dados referentes a venda de Janeiro/2024')
+
+st.text(f'Analise de {quantidade} cupons emitidos do produto selecionado')
+st.text(f'de um total de {qnt_total} cupons')
+st.text('Dados referentes a venda de 1B2024')
 st.dataframe(new_df2, use_container_width=True)
